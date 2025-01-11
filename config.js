bbb40400d451667a43d4582659e4979d6559cd73
Malvin King <malvink003@gmail.com>
2025-01-11 16:48:57 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -6,23 +6,51 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 ///=================When editing, do not accidentally delete anything !!MAIN-CONFIG=================================
-SESSION_ID: process.env.SESSION_ID || "jNUUFKiI#YfsBzKOAdNdt1s0efIXbPs_maWMGvOSDcYfejQOei2k",  // ADD Your Session Id 
+SESSION_ID: process.env.SESSION_ID || "jNUUFKiI#YfsBzKOAdNdt1s0efIXbPs_maWMGvOSDcYfejQOei2k",  // 📌 𝗚𝗲𝘁 𝗦𝗲𝘀𝘀𝗶𝗼𝗻 𝗶𝗱 𝗶𝗻 𝗣𝗮𝗶𝗿 𝗦𝗶𝘁𝗲 📌
 MODE: process.env.MODE || "public",  // Add Your Bot Mode (groups/privet/public)
 PREFIX: process.env.PREFIX || ".",   // Add Your Custom Prefix 
-OWNER_REACT: process.env.OWNER_REACT || "true", //add true or false
+OWNER_REACT: process.env.OWNER_REACT || "true", //📌 𝘁𝗿𝘂𝗲 𝗼𝗿 𝗳𝗮𝗹𝘀𝗲 𝗮𝘀 𝗬𝗼𝘂 𝗟𝗶𝗸𝗲 📌
 BOT_NAME: process.env.BOT_NAME || "𝙼𝙰𝙻𝚅𝙸𝙽 𝙼𝙳 v2",
-AUTO_VOICE: process.env.AUTO_VOICE || "true", //add true or false
-AUTO_STICKER: process.env.AUTO_STICKER || "true", //add true or false
-AUTO_REPLY: process.env.AUTO_REPLY || "true", //add true or false
-ANTI_LINK: process.env.ANTI_LINK || "true", //add true or false
-ANTI_BAD: process.env.ANTI_BAD || "true", //add true or false
-FAKE_RECORDING: process.env.FAKE_RECORDING || "true", //add true or false
-AUTO_REACT: process.env.AUTO_REACT || "true", //add true or false
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", //add true or false
-GOOGLE_CX: process.env.GOOGLE_CX || "AIzaSyD93IeJsouK51zjKgyHAwBIAlqr-a8mnME", 
-PASTEBIN_API_KEY: process.env.PASTEBIN_API_KEY || "uh8QvO6vQJGtIug9WvjdTAPx_ZAFJAxn",
+AUTO_VOICE: process.env.AUTO_VOICE || "true", //📌 𝘁𝗿𝘂𝗲 𝗼𝗿 𝗳𝗮𝗹𝘀𝗲 𝗮𝘀 𝗬𝗼𝘂 𝗟𝗶𝗸𝗲 📌
+AUTO_STICKER: process.env.AUTO_STICKER || "true", //📌 𝘁𝗿𝘂𝗲 𝗼𝗿 𝗳𝗮𝗹𝘀𝗲 𝗮𝘀 𝗬𝗼𝘂 𝗟𝗶𝗸𝗲 📌
+AUTO_REPLY: process.env.AUTO_REPLY || "true", //📌 𝘁𝗿𝘂𝗲 𝗼𝗿 𝗳𝗮𝗹𝘀𝗲 𝗮𝘀 𝗬𝗼𝘂 𝗟𝗶𝗸𝗲 📌
+ANTI_LINK: process.env.ANTI_LINK || "true", //📌 𝘁𝗿𝘂𝗲 𝗼𝗿 𝗳𝗮𝗹𝘀𝗲 𝗮𝘀 𝗬𝗼𝘂 𝗟𝗶𝗸𝗲 📌
+ANTI_BAD: process.env.ANTI_BAD || "true", //📌 𝘁𝗿𝘂𝗲 𝗼𝗿 𝗳𝗮𝗹𝘀𝗲 𝗮𝘀 𝗬𝗼𝘂 𝗟𝗶𝗸𝗲 📌
+FAKE_RECORDING: process.env.FAKE_RECORDING || "true", //📌 𝘁𝗿𝘂𝗲 𝗼𝗿 𝗳𝗮𝗹𝘀𝗲 𝗮𝘀 𝗬𝗼𝘂 𝗟𝗶𝗸𝗲 📌
+AUTO_REACT: process.env.AUTO_REACT || "true", //📌 𝘁𝗿𝘂𝗲 𝗼𝗿 𝗳𝗮𝗹𝘀𝗲 𝗮𝘀 𝗬𝗼𝘂 𝗟𝗶𝗸𝗲 📌
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", //📌 𝘁𝗿𝘂𝗲 𝗼𝗿 𝗳𝗮𝗹𝘀𝗲 𝗮𝘀 𝗬𝗼𝘂 𝗟𝗶𝗸𝗲 📌
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
 ///==================Do not change anything below=========================
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/7pg2gp.jpg", //do not change alive img url
 ALIVE_MSG: process.env.ALIVE_MSG || "ʜɪ ᴀᴍ ᴀʟɪᴠᴇ ɴᴏᴡ ᴀᴍ ᴍᴀʟᴠɪɴ xᴍᴅ ᴛʜᴇ ʙᴇsᴛ ᴡʜᴀᴛsᴀᴘᴘ ʙᴏᴛ🚀", // change alive msg if you wish
+
+//------------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------------
+
+  
+  
+MENU_IMG: process.env.MENU_IMG || "https://files.catbox.moe/7pg2gp.jpg",
+START_MSG: process.env.START_MSG || `ʙᴏᴛ⇨⇨ Connected Successfully!!!!!!* ✅ 
+
+ ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ᴍᴀʟᴠɪɴ xᴍᴅ
+
+*🔹 ᴘʀᴇғɪx:* ${prefix}
+
+*🔹 ᴏᴡɴᴇʀ:* 263714757857
+
+*🖇️Join My WhatsApp Channel✓💗* 
+
+- : https://whatsapp.com/channel/0029Vac8SosLY6d7CAFndv3Z
+
+*🖇️Share Bot Repo✓💗* 
+
+- : https://github.com/kingmalvn/MALVIN-XMD 
+
+*🖇️Subscribe My Youtube Channel✓💗*
+
+ - : https://www.youtube.com/malvintech2
+
+> *ᴘᴏᴡᴇʀᴇᴅ ʙʏ ʟᴏʀᴅ xᴍᴅ 🤹‍♂️ ʙᴇᴛᴀ ꜱᴇʀᴠɪᴄᴇ*`,
+
 };
